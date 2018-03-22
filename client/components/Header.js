c204c778b12a491ed4c53b7b04149d4fb0595bca
Albert Chang <albertchanged@gmail.com>
2018-03-21 19:55:05 -0700

    Implement App + Header components, CurrentUser query for logged in state

---
+++ client/components/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
+import { Link } from 'react-router';
+import mutation from '../mutations/LogOut';
+
+class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    if (loading) {
+      return <div />;
+    }
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Log Out</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo left">
+              Home
+            </Link>
+            <ul className="right">{this.renderButtons()}</ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
